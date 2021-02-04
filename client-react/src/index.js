45f2b70b22a5527e5d65b884efb62471e1fb5aa6
Gloria C <gloria_cheng7@yahoo.com>
2021-02-04 09:38:59 +0800

    updated react packages from dev branch

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import DrumMachine from './DrumMachine';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<DrumMachine />);
+// ReactDOM.createRoot(document.getElementById('root')).render(<DrumMachine />);
+ReactDOM.render(
+    <React.StrictMode>
+        <DrumMachine />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
