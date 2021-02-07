e4bef54a2c16994c538b2c2bbd4c6054e67507db
Gloria C <gloria_cheng7@yahoo.com>
2021-02-07 19:28:55 +0800

    added in first cut of user page - login, singup, view profile, edit, changepassword

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Redirect,
 } from "react-router-dom";
-// import { useState, useEffect } from "react";
 import Home from './pages/Home'
 import BeatSeq from './pages/BeatSeq'
 import BeatSeqTeaser from './pages/BeatSeqTeaser'
+import Login from './pages/account/Login'
+import SignUp from './pages/account/SignUp'
+import AccountView from './pages/account/AccountView'
+import AccountEdit from './pages/account/AccountEdit'
+import PasswordEdit from './pages/account/PasswordEdit'
 
 function App() {
+    const [loggedIn, setLoggedIn] = useState();
+    const [userId, setUserId] = useState(sessionStorage.getItem("userId"));
+
+    useEffect(() => {
+        console.log("App useEffect");
+        setUserId(sessionStorage.getItem("userId"));
+    }, [loggedIn]);
+
     return (
         <div>
+            {/* <NavBar loggedIn={loggedIn} /> */}
             <Router>
                 <Switch>
                     <Route exact path="/">
                         <Home />
                     </ Route>
+                    {/* <Route exact path="/restricted">
+                        <h1>You are not authorised to visit this page.</h1>
+                    </Route> */}
                     <Route exact path="/beatseq">
                         <BeatSeq />
                     </Route>
@@ -27,6 +43,27 @@ function App() {
                     <Route exact path="/teaser">
                         <BeatSeqTeaser />
                     </Route>
+                    <Route exact path="/login">
+                        <Login setLoggedIn={setLoggedIn} />
+                    </Route>
+                    <Route exact path="/user/new">
+                        <SignUp setLoggedIn={setLoggedIn} />
+                    </Route>
+                    <Route exact path="/user/:id">
+                        {/* {userId ? <AccountView /> : <Redirect to={"/login"} />} */}
+                        <AccountView />
+                    </Route>
+                    <Route exact path="/user/:id/edit">
+                        <AccountEdit />
+                    </Route>
+                    <Route exact path="/user/:id/changepassword">
+                        {/* {userId ? <PasswordEdit /> : <Redirect to={"/login"} />} */}
+                        <PasswordEdit />
+                    </Route>
+
+                    {/* <Route exact path="/logout">
+                        <Logout setLoggedIn={setLoggedIn} loggedIn={loggedIn} />
+                    </Route> */}
                 </Switch>
             </Router>
         </div>
@@ -38,28 +75,7 @@ export default App;
 {/*<div>
             <NavBar loggedIn={loggedIn} />
             <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route exact path="/user/new">
-                        <SignUp setLoggedIn={setLoggedIn} />
-                    </Route>
-                    <Route exact path="/login">
-                        <Login setLoggedIn={setLoggedIn} />
-                    </Route>
-                    <Route exact path="/logout">
-                        <Logout setLoggedIn={setLoggedIn} loggedIn={loggedIn} />
-                    </Route>
-                    <Route exact path="/user/:id">
-                        {userId ? <Account /> : <Redirect to={"/login"} />}
-                    </Route>
-                    <Route exact path="/user/:id/edit">
-                        <AccountEdit />
-                    </Route>
-                    <Route exact path="/user/:id/changepassword">
-                        {userId ? <PasswordEdit /> : <Redirect to={"/login"} />}
-                    </Route>
+                <Switch>     
                     <Route exact path="/user/:id/delete">
                         {userId ? (
                             <DeleteAccount setLoggedIn={setLoggedIn} />
@@ -67,14 +83,8 @@ export default App;
                                 <Redirect to={"/login"} />
                             )}
                     </Route>
-                    <Route exact path="/beatseq/:id">
-                        <BeatSeq />
-                    </Route>
                     <Route exact path="/info">
                         <Info />
-                    </Route>
-                    <Route exact path="/drumkit">
-                        <DrumKit />
                     </Route>
                 </Switch>
             </Router>
---
+++ client-react/src/pages/account/AccountView.js
@@ -0,0 +1,144 @@
+import { useState, useEffect } from "react";
+import { Link, Redirect, useParams } from "react-router-dom";
+import axios from "axios";
+import { Row, Col, Container, Button } from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+const Account = () => {
+    const [formData, setFormData] = useState({});
+    const userId = sessionStorage.getItem("userId");
+    const userIdParam = useParams().id;
+
+    useEffect(() => {
+        axios
+            .get(`/user/${userId}`)
+            .then((response) => {
+                setFormData(response.data);
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log("error", error);
+            });
+    }, []);
+
+    const keyWidth = 2;
+    const valueWidth = 5;
+
+    return (
+        <div>
+            {/* {userId === userIdParam ? ( */}
+            <div className="accForm">
+                <div id="accountdetail-form">
+                    <Row style={{ margin: "10px 0 2px 2px" }}>
+                        <h2>Account Details</h2>
+                    </Row>
+                    <Container style={{ margin: "5px" }}>
+                        <Row style={{ height: "50px" }}>
+                            <Col sm={keyWidth}>
+                                <span class="font-weight-bold">Username:</span>{" "}
+                            </Col>
+                            <Col sm={valueWidth}>{formData.username}</Col>
+                        </Row>
+                        <Row style={{ height: "50px" }}>
+                            <Col sm={keyWidth}>
+                                <span class="font-weight-bold">Password:</span>{" "}
+                            </Col>
+                            <Col sm={valueWidth}>xxxxxx</Col>
+                            <Col sm={2}>
+                                <div className="rightLink">
+                                    <Link to={`/user/${userId}/changepassword`}>
+                                        <Button
+                                            variant="outline-warning"
+                                            style={{
+                                                borderRadius: "25px",
+                                                padding: "6px",
+                                                width: "150px",
+                                                marginTop: "10px",
+                                                border: "3px solid",
+                                                fontWeight: "bold",
+                                            }}
+                                        >
+                                            Change
+                </Button>
+                                    </Link>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row style={{ height: "50px" }}>
+                            <Col sm={keyWidth}>
+                                <span class="font-weight-bold">Name:</span>{" "}
+                            </Col>
+                            <Col sm={valueWidth}>{formData.name}</Col>
+                        </Row>
+
+                        <Row style={{ height: "50px" }}>
+                            <Col sm={keyWidth}>
+                                <span class="font-weight-bold">Email:</span>{" "}
+                            </Col>
+                            <Col sm={valueWidth}>{formData.email}</Col>
+                        </Row>
+                    </Container>
+                </div>
+
+                <div id="accountDetail-buttonRow">
+                    <div className="rightLink">
+                        <Link to={`/user/${userId}/edit`}>
+                            <Button
+                                variant="outline-warning"
+                                style={{
+                                    borderRadius: "25px",
+                                    padding: "6px",
+                                    width: "150px",
+                                    marginTop: "10px",
+                                    border: "3px solid",
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                Edit Profile
+                </Button>
+                        </Link>
+                    </div>
+                    {/* <div className="rightLink">
+                        <Link to={`/user/${userId}/changepassword`}>
+                            <Button
+                                variant="outline-warning"
+                                style={{
+                                    borderRadius: "25px",
+                                    padding: "6px",
+                                    width: "150px",
+                                    marginTop: "10px",
+                                    border: "3px solid",
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                Change Password
+                </Button>
+                        </Link>
+                    </div> */}
+                    <div className="rightLink">
+                        <Link to={`/user/${userId}/delete`}>
+                            <Button
+                                variant="danger"
+                                style={{
+                                    borderRadius: "25px",
+                                    padding: "6px",
+                                    width: "150px",
+                                    marginTop: "10px",
+                                }}
+                            >
+                                Delete Account
+                </Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            {/* )
+              : (
+                     <Redirect to={"/about"} />
+                 )
+             } */}
+        </div >
+    );
+};
+
+export default Account;
