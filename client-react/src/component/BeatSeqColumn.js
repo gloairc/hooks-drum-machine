59323eba5004de6a965b51b90d0503fc904eca6b
Gloria C <gloria_cheng7@yahoo.com>
2021-02-09 16:44:02 +0800

    teaser page works, on save, alert logged in, after logged in, beatseqcol will trigger axios post, push into params and load page with beatseq that was working on

--- client-react/src/component/BeatSeqColumn.js
+++ client-react/src/component/BeatSeqColumn.js
@@ -1,27 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import BeatSeqList from "./BeatSeqList";
 const jwt = require("jsonwebtoken");
 
 const BeatSeqColumn = (props) => {
     const [machineCreated, setMachineCreated] = useState(false);
     const [newMachineId, setNewMachineId] = useState("");
-    // const userId = localStorage.getItem("userId");
-    // console.log("props at beatseqcol", props)
-    // const username = props.user.username
     const history = useHistory();
+    const beatseqId = useParams().id
     const token = localStorage.getItem("token");
     const decoded = jwt.verify(token, "sei-26"); //cant read secret :/
     const user = { userId: decoded.user._id, username: decoded.user.username };
 
     const handleAddMachineClick = () => {
-        setMachineCreated(false);
+        setMachineCreated(false);//so that can add again
         console.log("add new machine");
         const newMachineUser = {
             // username: user.username
             userId: user.userId,
         };
+
         axios
             .post("/api/beatSequence", newMachineUser)
             .then((response) => {
@@ -36,11 +35,39 @@ const BeatSeqColumn = (props) => {
             });
     };
 
+    useEffect(() => {//runs once when just log in
+        const tempbeatseq = JSON.parse(localStorage.getItem("tempbeatseq"));
+
+        if ((beatseqId === undefined && tempbeatseq !== null) || (beatseqId === "" && tempbeatseq !== null)) {//log in
+            //add new machine with tempbeatseq
+            const newMachineUser = {
+                // username: user.username
+                ...tempbeatseq,
+                userId: user.userId,
+            };
+            axios
+                .post("/api/beatSequence", newMachineUser)
+                .then((response) => {
+                    //remove localStorage
+                    localStorage.removeItem("tempbeatseq")
+                    console.log("posted a new machine to MongoDB from localStorage", response);
+                    //get the new object_id
+                    setNewMachineId(response.data._id);
+                    setMachineCreated(true);
+                })
+                .catch((error) => {
+                    console.log("add new machine error/error", error);
+                    // console.log("add new machine error/response", error.response.data.error);
+                });
+        };
+    }) // runs once
+
+
     useEffect(() => {
         if (machineCreated === true) {
             return history.push(`/beatseq/${newMachineId}`);
         }
-        setMachineCreated(false);
+        // setMachineCreated(false);
     }, [machineCreated]);
 
     return (
