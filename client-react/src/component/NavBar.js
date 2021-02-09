023bfa8bc9177e17e2968f2a32fbdbc4c5628e7d
Gloria C <gloria_cheng7@yahoo.com>
2021-02-09 11:47:01 +0800

    rearranged NavBar

--- client-react/src/component/NavBar.js
+++ client-react/src/component/NavBar.js
@@ -4,41 +4,10 @@ import { Link } from 'react-router-dom';
 import "../App.css"
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import { StatusProvider, useUser, useDispatch } from "./context/Context";
-const jwt = require("jsonwebtoken");
 
-const NavBar = ({ user }) => {// user={userId, userName}
-  // const [user, setUser] = useState(props.user) //from app which is triggered by login and logout
-  // console.log("props at Nav bar", props)
-  // console.log("props.user.userId at NavBar", props.user.userId)
-  // console.log("props.user.username at NavBar", props.user.username)
+const NavBar = ({ user }) => {// {userId, userName}
 
-  // let userNav = ""
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   if (token === undefined || token === "" || token === null) {
-  //     return
-  //   } else {
-  //     const decoded = jwt.verify(token, "sei-26");//cant read secret :/
-  //     userNav = { userId: decoded.user._id, username: decoded.user.username }
-  //     setUser(userNav)
-  //   }
-  // }, [props])
-
-  // useEffect(() => {//setUsername rerender when username change
-  //   //   if (Object.keys(props.user) !== 0) {
-  //   setUsername(props.user.username)
-  //   // setUserId(props.user.userId)
-  //   // };
-  // }, [props.user.username]);
-
-  // console.log("NavBar username", user)
   const loggedIn = user.userId === undefined ? false : true
-
-  // const handleClick = (event) => {//signup and logout
-  //   // setUserType(localStorage.getItem("userType"));
-  //   // setUserId(localStorage.getItem("userId"));
-  //   console.log("handle click event");
-  // };
 
   return (
     <Navbar
@@ -52,17 +21,17 @@ const NavBar = ({ user }) => {// user={userId, userName}
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
 
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-
-          <Nav.Link as={Link} to="/help">Help</Nav.Link>
-
+          {/* <Nav.Link as={Link} to="/">Home</Nav.Link> */}
           {loggedIn ? (
             <Nav.Link as={Link} to="/beatseq">Beat Sequencer</Nav.Link>
           ) : (
               // <Nav.Link onTouchCancel="/teaser">Teaser</Nav.Link>
               <Nav.Link as={Link} to="/teaser">Teaser</Nav.Link>
             )}
-          {loggedIn ? (<span id="welcome-name">Welcome</span>) : ""}
+
+          <Nav.Link as={Link} to="/help">Help</Nav.Link>
+
+          {loggedIn ? (<span id="welcome-name">Welcome {user.username}</span>) : ""}
           {loggedIn ? (
             <NavDropdown title="Account" id="basic-nav-dropdown">
               <NavDropdown.Item as={Link} to={`/user/${user.userId}`}>View Account</NavDropdown.Item>
